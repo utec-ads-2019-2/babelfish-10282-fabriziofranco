db093f7d520ed7ae2cb23a9ae17235f2e8cc6756
fabriziofranco <fabrizio.franco@utec.edu.pe>
2019-11-01 17:37:20 -0500

    Aceptado por debugger

---
+++ cmake-build-debug/CMakeFiles/babelfish_10282_fabriziofranco.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/babelfish_10282_fabriziofranco.dir/main.cpp.o"
+  "babelfish_10282_fabriziofranco"
+  "babelfish_10282_fabriziofranco.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/babelfish_10282_fabriziofranco.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string line, english,strange;
+    map<string,string> dictionary;
+
+    while(getline(cin,line)){
+        if(line.empty())
+            break;
+
+        stringstream ss(line);
+
+        ss >> english >> strange;
+
+        dictionary[strange] = english;
+    }
+    cout<<endl;
+
+    while(cin>>strange){
+        if(dictionary.find(strange)==dictionary.end())
+            cout<<"eh";
+        else
+            cout<<dictionary[strange];
+        cout<<endl;
+    }
+
+    return 0;
+}
